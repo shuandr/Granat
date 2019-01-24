f2390c91c3069871bdbb405471f4cb28b2239b49
shuandr <shuandr@gmail.com>
2019-01-24 13:44:42 +0200

    added shop page

---
+++ assets/js_plugins/myAngular.js
@@ -0,0 +1,34 @@
+var app = angular.module('shopGranat', ['ngRoute', 'ngSanitize']);
+
+
+
+app.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[{');
+    $interpolateProvider.endSymbol('}]}');
+});
+
+
+
+
+app.controller('shopGranat', function($scope, $http) {
+
+
+    $http.get("assets/json/products.json").then(function(response) {
+        $scope.products = response.data;
+
+    });
+
+    $http.get("assets/json/categories.json").then(function(response) {
+        $scope.categories = response.data;
+
+    });
+
+    
+
+
+
+
+
+
+
+});
