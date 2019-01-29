7eb5f8e41abd3003e0ee77df5f9a0f8c73d0941d
shuandr <shuandr@gmail.com>
2019-01-29 16:56:12 +0200

    accordion menu js fix & style

--- assets/js_plugins/myAngular.js
+++ assets/js_plugins/myAngular.js
@@ -23,12 +23,14 @@ app.controller('shopGranat', function($scope, $http) {
 
     });
 
-    
-
-
-
-
-
-
-
 });
+
+app.run(function($timeout){
+    $timeout(function() {
+        $(document).foundation();
+    }, 500);
+});
+
+/*app.run(function($rootScope){
+    $rootScope.$apply($(document).foundation());
+});*/
