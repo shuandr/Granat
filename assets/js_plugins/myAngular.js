4bc9bdadf55c317b3801684e3dd84cea46730617
shuandr <shuandr@gmail.com>
2019-04-03 21:41:06 +0300

    forget it :)

--- assets/js_plugins/myAngular.js
+++ assets/js_plugins/myAngular.js
@@ -25,11 +25,11 @@ app.controller('shopGranat', function($scope, $http) {
 
 });
 
-app.run(function($timeout){
+/*app.run(function($timeout){
     $timeout(function() {
         $(document).foundation();
     }, 500);
-});
+});*/
 
 /*app.run(function($rootScope){
     $rootScope.$apply($(document).foundation());
